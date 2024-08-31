275c00ccfabcef6165e10680214bdac5644a8932
Kanoe99 <kanoe.s99@gmail.com>
2024-08-31 22:45:40 +0500

    learn: pivot tables

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Tag;
 
 class Post extends Model
 {
@@ -12,6 +13,13 @@ class Post extends Model
     public function comments()
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function tags()
+    {
+        //2+ parameters here is unnecessary
+        //learning pivot table keys relationships
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'post_id', relatedPivotKey: 'tag_id');
     }
 
 }
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -7,6 +7,10 @@
             <li>
                 <a href="/jobs/{{ $job['id'] }}"
                     class="block px-8 py-4 border w-fit rounded-md border-gray-600 bg-gray-100 hover:bg-gray-300">
+                    <div
+                        class="mb-2 pb-2 text-lg font-semibold border-b-[1px] border-black text-blue-500 hover:underline">
+                        {{ $job->employer['name'] }}
+                    </div>
                     <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
                 </a>
             </li>
