b8405250edf0ba69bc71037950c9cf55267f27d8
Kanoe99 <kanoe.s99@gmail.com>
2024-09-04 14:17:41 +0500

    learn: seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Employer;
+use App\Models\Job;
+use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +18,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Comment::factory(10)->create();
+        Employer::factory(10)->create();
+        Post::factory(10)->create();
+        Tag::factory(10)->create();
+        User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => fake()->firstName(),
+        //     'surname' => fake()->lastName(),
+        //     'email' => fake()->email(),
+        // ]);
     }
 }
