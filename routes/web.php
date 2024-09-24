914b249d71daeaf3c87f3046b3d9202b324f0794
Kanoe99 <kanoe.s99@gmail.com>
2024-09-24 11:53:04 +0500

    struct: routes clean up

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::post('/jobs', [JobController::class, 'store']);
-//wild cards at the bottom!
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'delete']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
+Route::resource('jobs', JobController::class);
