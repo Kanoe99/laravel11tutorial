3403e53990a8ec25196d9da02dbeb1f87d03a948
Kanoe99 <kanoe.s99@gmail.com>
2024-09-25 13:45:51 +0500

    learn: things

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use App\Models\Employer;
+
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -44,5 +46,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
     }
 }
